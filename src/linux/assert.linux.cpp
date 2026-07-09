d9b395fdc786ef576ef44f577de2d2bab478cf0b
cakilgan <eneskobal18@gmail.com>
2026-07-10 02:41:57 +0300

    implement assertion

--- src/linux/assert.linux.cpp
+++ src/linux/assert.linux.cpp
@@ -4,3 +4,31 @@
 #include <omnix/platform/macros.h>
 #include <omnix/platform/detect.h>
 #include <omnix/platform/assert.h>
+
+#include <cstdio>
+#include <cstdlib>
+#include <unistd.h>
+#include <execinfo.h>
+
+using namespace ox;
+void default_assert_handler(cstr expr,
+                                   cstr file,
+                                   i32 line,
+                                   cstr msg)
+{
+    std::fprintf(stderr, "\033[1;31mASSERTION FAILED\033[0m\n");
+    std::fprintf(stderr, "Expr : %s\n", expr);
+    std::fprintf(stderr, "File : %s:%d\n", file, line);
+    std::fprintf(stderr, "Msg  : %s\n", msg ? msg : "none");
+
+#if OX_IS(PLATFORM,LINUX)
+    std::fprintf(stderr, "Stacktrace (top 10):\n");
+    void* bt[10];
+    int n = backtrace(bt, 10);
+    backtrace_symbols_fd(bt, n, STDERR_FILENO);
+#endif
+
+    std::abort();
+}
+
+ox::assert_handler ox::g_assert_handler = default_assert_handler;
