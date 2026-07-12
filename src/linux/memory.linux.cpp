3711dfc0df54f81f3242c73274266785dafb47a8
cakilgan <eneskobal18@gmail.com>
2026-07-12 18:29:04 +0300

    rename malloc to alloc, and pointer behavior changes

--- src/linux/memory.linux.cpp
+++ src/linux/memory.linux.cpp
@@ -5,7 +5,7 @@
 // Created by cakilgan on 7/10/26.
 //
 
-ox::result<ox::memory> ox::mem::malloc(bytes size, bytes alignment) {
+ox::result<ox::memory> ox::mem::alloc_as_memory(bytes size, bytes alignment) {
     OX_ASSERT(size != bytes(0));
     OX_ASSERT(alignment != bytes(0));
     if (size == bytes(0) || alignment == bytes(0)) return err::memory_size_is_zero;
@@ -21,14 +21,38 @@ ox::result<ox::memory> ox::mem::malloc(bytes size, bytes alignment) {
     return err::not_enough_memory;
 }
 
+ox::result<ox::pointer<void>> ox::mem::alloc(bytes size, bytes alignment) {
+    OX_ASSERT(size != bytes(0));
+    OX_ASSERT(alignment != bytes(0));
+    if (size == bytes(0) || alignment == bytes(0)) return err::memory_size_is_zero;
+
+    if (alignment == no_alignment) {
+        if (const auto pointer = ::malloc(size.ct))
+            return {pointer};
+        return err::not_enough_memory;
+    }
+
+    if (const auto pointer = ::aligned_alloc(alignment.ct,size.ct))
+        return {pointer};
+    return err::not_enough_memory;
+}
+
+ox::result_t ox::mem::free(vptr &ptr) {
+    OX_ASSERT(ptr != nullptr);
+    if (!ptr) return err::already_freed;
+    ::free(ptr.get());
+    ptr = nullptr;
+    return ok;
+}
+
 ox::result_t ox::mem::free(memory &mem, bytes alignment){
     OX_ASSERT(mem.pointer != nullptr);
     if (!mem.pointer) return err::already_freed;
     if (mem.size == bytes(0)) return err::memory_size_is_zero;
     if (alignment != no_alignment) {
-        free_aligned_sized(mem.pointer,alignment.ct,mem.size.ct);
+        free_aligned_sized(mem.pointer.get(),alignment.ct,mem.size.ct);
     }else {
-        free_sized(mem.pointer,mem.size.ct);
+        free_sized(mem.pointer.get(),mem.size.ct);
     }
     mem.pointer = nullptr;
     mem.size = bytes(0);
@@ -36,7 +60,7 @@ ox::result_t ox::mem::free(memory &mem, bytes alignment){
 }
 
 
-ox::result<ox::memory> ox::mem::vmalloc(bytes size) {
+ox::result<ox::memory> ox::mem::valloc_as_memory(bytes size) {
     OX_ASSERT(size != bytes(0));
     if (size == bytes(0)) return err::memory_size_is_zero;
 
@@ -50,13 +74,37 @@ ox::result<ox::memory> ox::mem::vmalloc(bytes size) {
     return memory{ptr,size};
 }
 
+ox::result<ox::pointer<void>> ox::mem::valloc(bytes size) {
+    OX_ASSERT(size != bytes(0));
+    if (size == bytes(0)) return err::memory_size_is_zero;
+
+    void* ptr = ::mmap(nullptr, size.ct,
+           PROT_READ | PROT_WRITE,
+           MAP_PRIVATE | MAP_ANONYMOUS | MAP_NORESERVE,
+           -1, 0);
+    if (ptr == MAP_FAILED) {
+        return err::not_enough_memory;
+    }
+    return {ptr};
+}
+
 ox::result_t       ox::mem::vfree(memory& o) {
     OX_ASSERT(o.pointer != nullptr);
     if (!o.pointer) return err::already_freed;
     if (o.size == bytes(0)) return err::memory_size_is_zero;
 
-    ::munmap(o.pointer,o.size.ct);
+    ::munmap(o.pointer.get(),o.size.ct);
     o.pointer = nullptr;
     o.size = bytes(0);
+    return ok;
+}
+
+ox::result_t ox::mem::vfree(vptr &o,bytes size) {
+    OX_ASSERT(o != nullptr);
+    if (!o) return err::already_freed;
+    if (size == bytes(0)) return err::memory_size_is_zero;
+
+    ::munmap(o.get(),size.ct);
+    o = nullptr;
     return ok;
 }
